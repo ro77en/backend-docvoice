954882db1d7ab45819171e0e66d89d8aaa64593a
ro77en <lucas.mdantas@icloud.com>
2025-06-25 13:30:04 -0300

    first commit

---
+++ src/main/java/br/com/doc_voice/DocVoiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.doc_voice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocVoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocVoiceApplication.class, args);
+	}
+
+}
